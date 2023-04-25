8c4a8284888ff9172b199ac4642646e94575257e
Kirill <mail@kirrr-ill.ru>
2023-04-25 19:17:02 +0300

    homework-11-done

---
+++ the_brand/data/data.js
@@ -0,0 +1,62 @@
+export const dataProducts = `[
+	{
+		"name": "ellery x&nbsp;m&rsquo;o capsule",
+		"image": "img/product_man-1.jpg",
+		"price": "$52.00",
+		"txt": "Known for her sculptural takes on traditional talioring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": 300,
+		"color": "red",
+		"size": "XL",
+		"quantity": "2"
+	},
+	{
+		"name": "ellery x&nbsp;m&rsquo;o capsule",
+		"image": "img/product_woman-1.png",
+		"price": "$52.00",
+		"txt": "Known for her sculptural takes on traditional talioring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": 300,
+		"color": "red",
+		"size": "XL",
+		"quantity": "2"
+	},
+	{
+		"name": "ellery x&nbsp;m&rsquo;o capsule",
+		"image": "img/product_man-2.jpg",
+		"price": "$52.00",
+		"txt": "Known for her sculptural takes on traditional talioring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": 300,
+		"color": "red",
+		"size": "XL",
+		"quantity": "2"
+	},
+	{
+		"name": "ellery x&nbsp;m&rsquo;o capsule",
+		"image": "img/product_man-3.png",
+		"price": "$52.00",
+		"txt": "Known for her sculptural takes on traditional talioring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": 300,
+		"color": "red",
+		"size": "XL",
+		"quantity": "2"
+	},
+	{
+		"name": "ellery x&nbsp;m&rsquo;o capsule",
+		"image": "img/product_woman-2.png",
+		"price": "$52.00",
+		"txt": "Known for her sculptural takes on traditional talioring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": 300,
+		"color": "red",
+		"size": "XL",
+		"quantity": "2"
+	},
+	{
+		"name": "ellery x&nbsp;m&rsquo;o capsule",
+		"image": "img/product_woman-3.png",
+		"price": "$52.00",
+		"txt": "Known for her sculptural takes on traditional talioring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": 300,
+		"color": "red",
+		"size": "XL",
+		"quantity": "2"
+	}
+]`;
