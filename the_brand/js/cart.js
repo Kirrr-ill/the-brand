9afcc4a7be84c3f8aa809b436e90212914cf267f
Kirill <mail@kirrr-ill.ru>
2023-05-04 00:42:13 +0300

    homework-12-done

--- the_brand/js/cart.js
+++ the_brand/js/cart.js
@@ -1,5 +1,5 @@
 
-const basketBox = document.querySelector('.cart-box');
+const cartBox = document.querySelector('.cart-box');
 const addItem = document.querySelectorAll('.product-card-btn');
 
 const cartTitle = document.createElement('h2');
@@ -8,21 +8,28 @@ cartTitle.classList.add('cart-items-title');
 
 addItem.forEach((element) => {
 	element.addEventListener('click', () => {
-		const basketItem = element.closest('.product-card-items');
-		const itemInfo = {
-			id: basketItem.dataset.id,
-			image: basketItem.querySelector('.product-card-image').getAttribute('src'),
-			name: basketItem.querySelector('.product-card-title').innerText,
-			price: basketItem.querySelector('.product-card-price').innerText,
+		const cartItem = element.closest('.product-card-items');
+		const cardInfo = {
+			id: cartItem.dataset.id,
+			image: cartItem.querySelector('.product-card-image').getAttribute('src'),
+			name: cartItem.querySelector('.product-card-title').innerText,
+			price: cartItem.querySelector('.product-card-price').innerText,
 		};
-		const basketEl = `
-			<div class="product" data-id="${itemInfo.id}">
+
+		const itemInCart = cartBox.querySelector(`[data-id="${cardInfo.id}"]`);
+		if (itemInCart) {
+			// const value = itemInCart.querySelector('.input-qty').value;
+			// const quantity = Number(value);
+			// console.log(typeof quantity);
+		} else {
+			const cartEl = `
+			<div class="product" data-id="${cardInfo.id}">
 				<button class="btn-del" type="button">Удалить</button>
 					<div class="product-content">
-						<img class = "product-img" src="${itemInfo.image}" alt="${itemInfo.name}">
+						<img class = "product-img" src="${cardInfo.image}" alt="${cardInfo.name}">
 						<div class="product-info">
-							<h2 class="product-name">${itemInfo.name}</h2>
-							<p class="product-price-label">Price: <span class="product-price">${itemInfo.price}</span></p>
+							<h2 class="product-name">${cardInfo.name}</h2>
+							<p class="product-price-label">Price: <span class="product-price">${cardInfo.price}</span></p>
 							<p class="product-color-label">Color: RED</p>
 							<p class="product-size-label">Size: XL</p>
 							<div class="product-qty-label">
@@ -33,17 +40,20 @@ addItem.forEach((element) => {
 					</div>
 				</div>
 		`;
-		basketBox.insertAdjacentHTML('beforeend', basketEl);
-		if (basketBox.children.length > 1) {
-			basketBox.appendChild(cartTitle);
-		};
-		basketBox.insertAdjacentElement('afterbegin', cartTitle);
+			cartBox.insertAdjacentHTML('beforeend', cartEl);
+		}
+		if (cartBox.children.length > 1) {
+			cartBox.appendChild(cartTitle);
+		}
+		cartBox.insertAdjacentElement('afterbegin', cartTitle);
+
+
 		const delBtn = document.querySelectorAll('.btn-del');
 		delBtn.forEach(button => {
 			button.addEventListener('click', () => {
 				const product = button.closest('.product');
 				product.remove();
-				if (basketBox.children.length === 1) {
+				if (cartBox.children.length === 1) {
 					cartTitle.remove();
 				};
 			});
